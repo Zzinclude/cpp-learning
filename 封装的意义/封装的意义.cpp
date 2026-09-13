4f1e9d2e3d5f85ab18c05e2f8c4b5e9af5c1a1a1
神奇的cheems <3418323655@qq.com>
2026-09-13 21:41:51 +0800

    封装的意义

---
+++ 封装的意义/封装的意义.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+using namespace std;
+//圆周率pi=3.14
+const double pi = 3.14;
+//设计一个圆类，求圆的周长
+//周长公式：2*pi*半径
+
+//class代表设计一个类，类后面紧紧跟着就是类的名称
+class circle {
+	//访问权限
+public://公共权限
+
+	//属性
+	int r;//半径
+	//行为
+	double calculateC() {
+		return 2 * pi * r;
+	}
+};
+
+int main() {
+	//通过圆类 创建一个具体的圆
+	//实例化
+	circle ci;
+	//给圆对象的属性赋值
+	ci.r = 10;
+	cout << "圆的周长为：" << ci.calculateC() << endl;
+	system("pause");
+	return 0;
+}
